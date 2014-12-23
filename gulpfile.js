bebd081c7853397d35f0c4899e8ee31b6aebc439
bornkiller <hjj491229492@hotmail.com>
2014-12-23 23:16:44 +0800

    modify build way.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var mocha = require('gulp-mocha');
 var through = require('./through-gulp');
 
 gulp.task('jshint', function() {
@@ -10,20 +9,11 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
-
-gulp.task('test', ['jshint'], function () {
-    return gulp.src(['test/main.js', 'test/extends.js'], {read: false})
-        .pipe(mocha());
-});
-
 gulp.task('example', function() {
 	return gulp.src(['package.json'])
-	    .pipe(through(function(file, encoding, callback) {
-	    	/*
-	    	 * process the file whatever necessary
-	    	 */
-	    	this.push(file);
-	    	callback();
-	    }))
-	    .pipe(gulp.dest('example'));
+    .pipe(through(function(file, encoding, callback) {
+     	this.push(file);
+	  	callback();
+	  }))
+	  .pipe(gulp.dest('example'));
 });
