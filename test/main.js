2a6adc0882ac2c60c8d3eca284232e3f0313a3f6
bornkiller <hjj491229492@hotmail.com>
2014-12-23 23:47:43 +0800

    modify dependency into latest and indent.

--- test/main.js
+++ test/main.js
@@ -5,121 +5,120 @@ var assert = require('stream-assert');
 var through = require('../through-gulp.js');
 var PluginError = gutil.PluginError;
 var ERROR_NAME = 'through-gulp';
-require('should');
-require('mocha');
+var should = require('should');
+
 describe('through-gulp', function () {
+  it('should implement transform class', function () {
+    var expect = through(function(){}, function() {});
+    (expect instanceof transform).should.be.true;
+    (expect._transform).should.be.a.Function;
+    (expect._flush).should.be.a.Function;
+  });
 
-    it('should implement transform class', function () {
-        var expect = through(function(){}, function() {});
-        (expect instanceof transform).should.be.true;
-        (expect._transform).should.be.a.Function;
-        (expect._flush).should.be.a.Function;
+  it('should receive data from gulp', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+        (file.isBuffer()).should.be.true;
+        (file.isStream()).should.be.false;
+         callback();
+         done();
+      }))
     });
 
-    it('should receive data from gulp', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                (file.isBuffer()).should.be.true;
-                (file.isStream()).should.be.false;
-                callback();
-                done();
-            }))
+  it('should pass data to next when undeclared', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through())
+      .pipe(assert.first(function(file) {
+        (file.isBuffer()).should.be.true;
+        (file.isStream()).should.be.false;
+        done();
+      }))
     });
 
-    it('should pass data to next when undeclared', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through())
-            .pipe(assert.first(function(file) {
-                (file.isBuffer()).should.be.true;
-                (file.isStream()).should.be.false;
-                done();
-            }))
+  it('should pass data to next when declared', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+        this.push(file);
+        callback();
+      }))
+      .pipe(assert.first(function(file) {
+        (file.isBuffer()).should.be.true;
+        (file.isStream()).should.be.false;
+        done();
+      }))
     });
 
-    it('should pass data to next when declared', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                this.push(file);
-                callback();
-            }))
-            .pipe(assert.first(function(file) {
-                (file.isBuffer()).should.be.true;
-                (file.isStream()).should.be.false;
-                done();
-            }))
+  it('should support single data transmission', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+        this.push(file);
+        callback();
+      }))
+      .pipe(assert.first(function(file) {
+        (file.contents.toString()).should.equal('define({});');
+      }))
+      .pipe(assert.end(done));
     });
 
-    it('should support single data transmission', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                this.push(file);
-                callback();
-            }))
-            .pipe(assert.first(function(file) {
-                (file.contents.toString()).should.equal('define({});');
-            }))
-            .on('end', done);
+  it('should support single data transmission', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+          callback();
+        }, function(callback) {
+          this.push('beautiful');
+          callback();
+      }))
+      .pipe(assert.first(function(file) {
+        file.toString().should.equal('beautiful')
+      }))
+      .pipe(assert.end(done));
     });
 
-    it('should support single data transmission', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                callback();
-            }, function(callback) {
-                this.push('beautiful');
-                callback();
-            }))
-            .pipe(assert.first(function(file) {
-                file.toString().should.equal('beautiful')
-            }))
-            .on('end', done);
+  it('should support multi data transmission', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+           this.push(file);
+           callback();
+        }), function(callback) {
+           this.push('beautiful');
+           callback();
+      })
+      .pipe(assert.first(function(file) {
+        (file.contents.toString()).should.equal('define({});');
+      }))
+      .pipe(assert.second(function(file) {
+        (file.toString()).should.equal('beautiful');
+      }))
+      .pipe(assert.end(done))
     });
 
-    it('should support multi data transmission', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                this.push(file);
-                callback();
-            }), function(callback) {
-                this.push('beautiful');
-                callback();
-            })
-            .pipe(assert.first(function(file) {
-                (file.contents.toString()).should.equal('define({});');
-            }))
-            .pipe(assert.second(function(file) {
-                (file.toString()).should.equal('beautiful');
-            }))
-            .on('end', done);
+  it('should support file process', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+        var sample = new Buffer('love ');
+        file.contents = Buffer.concat([sample, file.contents]);
+        this.push(file);
+        callback();
+      }))
+      .pipe(assert.first(function(file) {
+        (file.contents.toString()).should.equal('love define({});');
+      }))
+      .pipe(assert.end(done));
     });
 
-    it('should support file process', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                var sample = new Buffer('love ');
-                file.contents = Buffer.concat([sample, file.contents]);
-                this.push(file);
-                callback();
-            }))
-            .pipe(assert.first(function(file) {
-                (file.contents.toString()).should.equal('love define({});');
-            }))
-            .on('end', done);
-    });
-
-    it('should support event emit', function (done) {
-        gulp.src('./test/fixtures/template.js')
-            .pipe(through(function(file, encoding, callback) {
-                var error = new PluginError(ERROR_NAME, 'missing something');
-                this.emit('error', error);
-                callback();
-            }, function(callback) {
-                this.push('beautiful');
-                callback();
-            }))
-            .on('error', function(err) {
-                err.should.be.a.Error;
-                done();
-            });
+  it('should support event emit', function (done) {
+    gulp.src('./test/fixtures/template.js')
+      .pipe(through(function(file, encoding, callback) {
+          var error = new PluginError(ERROR_NAME, 'missing something');
+          this.emit('error', error);
+          callback();
+        }, function(callback) {
+          this.push('beautiful');
+          callback();
+      }))
+      .on('error', function(err) {
+        err.should.be.an.Error;
+        done();
+      })
     });
 });
--- through-gulp.js
+++ through-gulp.js
@@ -1,52 +1,66 @@
+/**
+ * Expose function to generate transform stream
+ * @modules through-gulp
+ * @versions v0.3.5
+ * @typedef {object} transformStream;
+ */
+
+/**
+ * Module dependencies
+ */
 var util = require('util');
 var Transform = require('stream').Transform;
-var throughGulp;
+var through;
 
-throughGulp = function(transformFunction, flushFunction) {
-    var transform;
-    var flush;
+/**
+ * Exposed function generate transform stream
+ * @param {function} transformFunction
+ * @param {function} flushFunction
+ * @returns {transformStream}
+ */
+through = function(transformFunction, flushFunction) {
+  var transform;
+  var flush;
 
-    if (typeof transformFunction !== 'function') {
-        transform = function(file, encoding, callback) {
-            this.push(file);
-            callback();
-        }
-    } else {
-        transform = transformFunction;
-    }
+  // use default function when not pass in
+  transform = typeof transformFunction === 'function' ?
+    transformFunction :
+    function(file, encoding, callback) {
+      this.push(file);
+      callback();
+    };
 
-    if (typeof flushFunction !== 'function') {
-        flush = function(callback) {
-            callback();
-        };
-    } else {
-        flush = flushFunction;
-    }
+  flush = typeof flushFunction === 'function' ?
+    flushFunction :
+    function(callback) {
+      callback();
+    };
 
-    util.inherits(Through, Transform);
-    function Through() {
-      Transform.call(this, {objectMode: true});
-    }
-    Through.prototype._transform = transform;
-    Through.prototype._flush = flush;
+  // implement transform stream
+  util.inherits(Through, Transform);
+  function Through() {
+    Transform.call(this, {objectMode: true});
+  }
+  Through.prototype._transform = transform;
+  Through.prototype._flush = flush;
 
-    return new Through();
+  return new Through();
 };
 
-throughGulp.map = function(fileMaps) {
-    return throughGulp(function(file, encoding, callback) {
-        this.push(fileMaps(file));
-        callback();
-    });
+through.map = function(fileMaps) {
+  return through(function(file, encoding, callback) {
+    this.push(fileMaps(file));
+    callback();
+  });
 };
 
-throughGulp.filter = function(fileFilter) {
-    return throughGulp(function(file, encoding, callback) {
-        if (fileFilter(file)) {
-            this.push(file);
-        }
-        callback();
-    });
+through.filter = function(fileFilter) {
+  return through(function(file, encoding, callback) {
+    if (fileFilter(file)) {
+      this.push(file);
+    }
+    callback();
+  });
 };
 
-exports = module.exports = throughGulp;
+exports = module.exports = through;
